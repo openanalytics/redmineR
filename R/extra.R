af1213da0a8e94d80dcb0c341b777b916f04c34e
mnazarov <maxim.nazarov@openanalytics.eu>
2017-07-04 17:38:08 +0200

    docs for 'extra' funcs

--- R/extra.R
+++ R/extra.R
@@ -1,30 +1,50 @@
 ## 
 
+#' Show info on extra endpoints
+#' 
+#' Convenience functions to list endpoints, in particular to get ids to be 
+#' used in creating/updating or listing issues or projects.
+#' @return a data frame
+#' @author Maxim Nazarov
+#' @export
 redmine_trackers <- function() {
   redmine_get_all_pages("trackers")
 }
 
+#' @rdname redmine_trackers
+#' @export
 redmine_issue_statuses <- function() {
   redmine_get_all_pages("issue_statuses")
 }
 
+#' @rdname redmine_trackers
+#' @export
 redmine_users <- function() {
   redmine_get_all_pages("users")
 }
 
+#' @rdname redmine_trackers
+#' @export
 redmine_time_entries <- function() {
   redmine_get_all_pages("time_entries")
 }
 
 # enumerations
+
+#' @rdname redmine_trackers
+#' @export
 redmine_issue_priorities <- function() {
   redmine_get_all_pages("enumerations/issue_priorities")
 }
 
+#' @rdname redmine_trackers
+#' @export
 redmine_time_entry_activities <- function() {
   redmine_get_all_pages("enumerations/time_entry_activities")
 }
 
+#' @rdname redmine_trackers
+#' @export
 redmine_document_categories <- function() {
   redmine_get_all_pages("enumerations/document_categories")
 }
